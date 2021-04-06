c5c261015e2256fe97fb4e87f718aec0f541ff23
ma-reb <marcos.reboucas-ext@viavarejo.com.br>
2021-04-06 09:22:05 -0300

    Created Feed Item model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Marcos Rebouças on 06/04/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
