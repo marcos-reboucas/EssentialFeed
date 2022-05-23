9625065743e097e4f01b4d512a656678a684db6d
marcos-reboucas <contact@marcosreboucas.com>
2022-05-23 12:22:39 -0300

    Add comments on MVP view models role and differences between MVVM view models.

--- EssentialFeed/EssentialFeediOS/Feed UI/Models/FeedPresenter.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Models/FeedPresenter.swift
@@ -7,6 +7,8 @@
 
 import EssentialFeed
 
+// Criamos essas 2 structs com view models (FeedLoadingViewModel e FeedViewModel) substituindo os types Bool e [FeedImage] que eram passados diretamente antes. Fizemos isso para clarificar a comunicação entre a presentation layer e a UI layer. As view models são 'pure data'. Em MVP as view models, também chamadas de ViewData ou PresentationModel, carregam apenas Data para a view que está sendo apresentada, e não possuem behavior. Isso difere do MVVM onde as view models tem dependencias e behavior.
+
 struct FeedLoadingViewModel {
     let isLoading: Bool
 }
