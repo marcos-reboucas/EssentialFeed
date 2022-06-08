f740816c43ef76afc861ab13c51982bf95b71f19
marcos-reboucas <contact@marcosreboucas.com>
2022-06-08 11:03:11 -0300

    Move main thread dispatch to the Composition layer with a Decorator.

--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -12,7 +12,7 @@ public final class FeedUIComposer {
     private init() {}
     
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
-        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader)
+        let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: MainQueueDispatchDecorator(decoratee: feedLoader))
         
         let feedController = FeedViewController.makeWith(
             delegate: presentationAdapter,
@@ -23,6 +23,29 @@ public final class FeedUIComposer {
             loadingView: WeakRefVirtualProxy(feedController))
         
         return feedController
+    }
+}
+
+// The Decorator pattern is used to add behavior to an instance while keeping the same interface. In this example we want FeedLoader instance to be dispatched in the main thread (this is the added behavior). MainQueueDispatchDecorator also need to conform to FeedLoader because it's going to behave as a FeedLoader, forwarding the messages to the 'decoratee'. The decorator add behavior to the instance without changing the instance, that's the SOLID Open Closed Principle (we're adding behavior without changing code). Here we 'decorate' all the instances of FeedLoader in a single place (we have just one screen and one instance of FeedLoader in our app but image if we had 50 screens, this thread checking would be spread all over the code).
+
+private class MainQueueDispatchDecorator: FeedLoader {
+    
+    private let decoratee: FeedLoader
+    
+    init(decoratee: FeedLoader) {
+        self.decoratee = decoratee
+    }
+    
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load { result in
+            if Thread.isMainThread {
+                completion(result)
+            } else {
+                DispatchQueue.main.async {
+                    completion(result)
+                }
+            }
+        }
     }
 }
 
