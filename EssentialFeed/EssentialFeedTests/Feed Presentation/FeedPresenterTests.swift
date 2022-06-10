bfcd0b163038377f3a932492a4f1543328ec2f6f
marcos-reboucas <contact@marcosreboucas.com>
2022-06-10 10:36:55 -0300

    Extract system under test creation to a test factory method

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -6,7 +6,8 @@
 //
 
 import XCTest
-@testable import EssentialFeediOS
+import EssentialFeed
+//@testable import EssentialFeediOS
 
 final class FeedPresenter {
     init(view: Any) {
@@ -18,18 +19,24 @@ class FeedPresenterTests: XCTestCase {
     
     // We recomend to start from the degenerate, simple and trivial behaviors first. In this case, simply making sure the FeedPresenter doesn't perform any work in the constructor (the init method doesn't have any behavior, that's what we want: "no behaviors in initializers").
     func test_init_doesNotSendMessagesToView() {
-        let view = ViewSpy()
-        
-        _ = FeedPresenter(view: view)
+        let (_, view) = makeSUT()
         
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
-}
+    
+    // MARK: - Helpers
 
-// MARK: - Helpers
+    // Factory Method
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedPresenter(view: view)
+        trackForMemoryLeaks(view, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, view)
+    }
 
-// We create a view instance and for this case we need a Spy so we can investigate which messages the view received.
-
-private class ViewSpy {
-    let messages = [Any]()
+    // We create a view instance and for this case we need a Spy so we can investigate which messages the view received.
+    private class ViewSpy {
+        let messages = [Any]()
+    }
 }
